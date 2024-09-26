c6d88add4f28258fb408e4f247a3c567d779f54f
J-Goleath <jgoleath60816@ufide.ac.cr>
2024-09-25 20:05:14 -0600

    Creacion de Proyecto
    Se creo proyecto desde 0

---
+++ Tienda_InitializrSpringbootProject/src/main/java/Tienda/demo/TiendaJefryGoleathApplication.java
@@ -0,0 +1,13 @@
+package Tienda.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaJefryGoleathApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaJefryGoleathApplication.class, args);
+	}
+
+}
