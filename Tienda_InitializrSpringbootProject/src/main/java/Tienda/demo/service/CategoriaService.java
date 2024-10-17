d8ebb2a7d0737575a65f5214e25fd1caced506dc
J-Goleath <jgoleath60816@ufide.ac.cr>
2024-10-16 20:52:06 -0600

    EOC W05
    End of class Week 05

---
+++ Tienda_InitializrSpringbootProject/src/main/java/Tienda/demo/service/CategoriaService.java
@@ -0,0 +1,11 @@
+package Tienda.demo.service;
+
+import Tienda.demo.domain.Categoria;
+import java.util.List;
+
+public interface CategoriaService {
+    
+    // Se obtiene un listado de categorias en un List
+    public List<Categoria> getCategorias(boolean activos);
+ 
+}
