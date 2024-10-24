d044eea6d4533cf3b46e0fa94e08db0b228d7d12
J-Goleath <jgoleath60816@ufide.ac.cr>
2024-10-23 20:55:35 -0600

    EOC W06
    End of class

---
+++ Tienda_InitializrSpringbootProject/src/main/java/Tienda/demo/service/FirebaseStorageService.java
@@ -0,0 +1,20 @@
+package Tienda.demo.service;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public interface FirebaseStorageService {
+
+    public String cargaImagen(MultipartFile archivoLocalCliente, String carpeta, Long id);
+
+    //El BuketName es el <id_del_proyecto> + ".appspot.com"
+    final String BucketName = "techshop-2ddd7.appspot.com";
+
+    //Esta es la ruta básica de este proyecto Techshop
+    final String rutaSuperiorStorage = "techshop";
+
+    //Ubicación donde se encuentra el archivo de configuración Json
+    final String rutaJsonFile = "firebase";
+    
+    //El nombre del archivo Json
+    final String archivoJsonFile = "techshop-2ddd7-firebase-adminsdk-hksxr-35faf0ece6.json";
+}
